220dead1e523e24ea2e0d41d2da8350715367240
SethWarren120 <sethwarren120@gmail.com>
2024-01-23 18:39:10 -0800

    Update Gyro.java

--- src/main/java/frc/robot/sensor/pose/Gyro.java
+++ src/main/java/frc/robot/sensor/pose/Gyro.java
@@ -1,5 +1,45 @@
 package frc.robot.sensor.pose;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Robot;
+
 public class Gyro {
-    
+    public enum Mode {
+        Primary,
+        Secondary,
+        Estimation
+    }
+
+    public static Mode mode = Mode.Primary;
+
+    private static Pigeon2 pigeon;
+    // TODO - Implement NavX support once the library updates to 2024
+
+    static {
+        if (Robot.isSimulation()) {
+            mode = Mode.Estimation;
+        } else {
+            try {
+                pigeon = new Pigeon2(0);
+            }
+            catch (Exception e) {
+                mode = Mode.Secondary;
+                System.out.println("Failed to connect to primary gyro");
+//                try {
+//                    // Instantiate NavX
+//                }
+//                catch (Exception ex) {
+                mode = Mode.Estimation;
+                System.out.println("Failed to connect to secondary gyro");
+//                }
+            }
+        }
+    }
+
+    public static Rotation2d getHeading() {
+        return Rotation2d.fromDegrees(pigeon.getYaw().getValue());
+    }
+    public static Rotation2d getPitch() { return Rotation2d.fromDegrees(pigeon.getPitch().getValue()); }
+    public static Rotation2d getRoll() { return Rotation2d.fromDegrees(pigeon.getRoll().getValue()); }
 }
