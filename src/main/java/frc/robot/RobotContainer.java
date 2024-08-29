082fa6749b513ad5b1ec3fdb34c5d4e95d0109a7
kaiyamashita08 <114874647+kaiyamashita08@users.noreply.github.com>
2024-08-29 00:39:38 -0700

    made vision work with odometry hopefully

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,6 +53,8 @@ public class RobotContainer {
         switch (Constants.mode) {
             case REAL -> {
                 drive = new Drive(
+                        // TODO: Figure out id
+                        new VisionIOCamera("Front_Camera"),
                         new GyroIOPigeon2(10),
                         new ModuleIOSparkMax(0),
                         new ModuleIOSparkMax(1),
@@ -77,6 +79,7 @@ public class RobotContainer {
 
             case SIM -> {
                 drive = new Drive(
+                        new VisionIO(),
                         new GyroIO(),
                         new ModuleIOSim(),
                         new ModuleIOSim(),
@@ -101,6 +104,7 @@ public class RobotContainer {
 
             case REPLAY -> {
                 drive = new Drive(
+                        new VisionIO(),
                         new GyroIO(),
                         new ModuleIO(),
                         new ModuleIO(),
--- src/main/java/frc/robot/subsystems/drive/VisionIO.java
+++ src/main/java/frc/robot/subsystems/drive/VisionIO.java
@@ -2,13 +2,20 @@ package frc.robot.subsystems.drive;
 
 import org.littletonrobotics.junction.AutoLog;
 
-import edu.wpi.first.math.geometry.Pose3d;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import java.util.List;
 
 public class VisionIO {
+
     @AutoLog
     public static class VisionIOInputs {
-        public boolean isConnected = false;
-        public Pose3d pose3d;
+        public boolean hasTargets = false;
+        public boolean hasEstimatedPose = false;
+        public EstimatedRobotPose estimatedPose = null;
+        public PhotonTrackedTarget bestTarget = null;
+        public List<PhotonTrackedTarget> targets = null;
     }
 
     public void updateInputs(VisionIOInputs inputs) {
