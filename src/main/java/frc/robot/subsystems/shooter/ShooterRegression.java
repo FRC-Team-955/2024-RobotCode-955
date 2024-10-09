9daa1ffc9d9c56ea639dabcbd1b5cee2ee3c3e39
amsam0 <44983869+amsam0@users.noreply.github.com>
2024-10-08 19:54:12 -0700

    Start shooter data collection

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterRegression.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.shooter;
+
+public final class ShooterRegression {
+    /*
+    distance = distance from center of robot to speaker wall
+    distance from front of subwoofer to speaker wall = 36 in
+
+    distance | shooter angle (degrees) | shooter speed (RPM)
+    ---------|-------------------------|--------------------
+    52.5 in  | 0                       | 0
+    */
+}
